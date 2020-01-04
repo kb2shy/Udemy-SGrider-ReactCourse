2cf2501bfb9396dc97460e093ee45c5022bc0ece
Chhaian Pin <chhaianpin@hotmail.com>
2020-01-03 16:17:52 -0800

    created ApprovalCard component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard />
             <CommentDetail 
                 avatar={faker.image.avatar()} 
                 name="Sam" 
